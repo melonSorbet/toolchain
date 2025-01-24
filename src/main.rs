2b2558f59d04a846cbc79ce493306234e3a621b8
melonSorbet <kilicyunusemre678@gmail.com>
2025-01-25 00:09:51 +0100

    feature: add clap for command line argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use clap::Parser;
+
+#[derive(Parser)]
 struct Command{
-    commands: String,
-    sub_commands: Vec<String>,
+    command: String,
+    name: String
 }
+
+
 // TODO: implement smart and enhancible way to add more commands.
 fn main(){
 
     // TODO: read in all the arguments. With clap or own implementation.
     // TODO: take arguments and parse them into the correct command-function
-    
-    println!("no arguments given!")
+    let args = Command::parse();
+
+
+    println!("this is the command you want to execute {} ",args.command);
+    println!("this is the name you want the command to have {} ",args.name);
 }
