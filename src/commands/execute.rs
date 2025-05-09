96529fbf14b2b40f3dfff2e6e85a7cd10637b673
melonSorbet <kilicyunusemre678@gmail.com>
2025-05-09 21:27:55 +0200

    fix: add error handling to get shell name

--- src/commands/execute.rs
+++ src/commands/execute.rs
@@ -1,6 +1,6 @@
 use crate::services::database;
 use clap::builder::Str;
-use clap::Args;
+use clap::{Args, Error};
 use procfs::process::Process;
 use std::process::Command;
 // TODO: implement command execution as well as subcommand execution, add how often a command should be played
@@ -16,31 +16,35 @@ pub async fn execute(execute_command: ExecuteCommand) {
     let commands = database::find_all_subcommands(&pool, &execute_command.name)
         .await
         .unwrap();
+    let shell = get_current_shell().unwrap();
     for command in commands {
-        Command::new(command.command).output().expect("");
+        let current_command = format!("{} -c {}", shell, command.command);
+        Command::new(current_command).output().expect("");
     }
 }
 
-fn get_current_shell() {
-    let me = Process::myself().unwrap();
-    println!("Current process: {:?}", me.stat());
+fn get_current_shell() -> Result<String, Box<dyn std::error::Error>> {
+    let me = Process::myself()?;
+    println!("Current process: {:?}", me.stat()?);
 
-    // Get parent PID (Unix-like systems)
     #[cfg(unix)]
     {
         let parent_pid = unsafe { libc::getppid() };
         println!("Parent PID: {}", parent_pid);
 
-        // Try to get parent process info
         if let Ok(parent_proc) = Process::new(parent_pid) {
-            println!("Parent process: {:?}", parent_proc.stat().unwrap().comm);
+            let parent_stat = parent_proc.stat()?;
+            println!("Parent process: {:?}", parent_stat.comm);
+            return Ok(parent_stat.comm);
         } else {
             println!("Failed to get parent process info (may have exited)");
+            return Err("Failed to get parent process".into());
         }
     }
 
     #[cfg(not(unix))]
     {
         println!("Parent PID detection is only supported on Unix-like systems");
+        return Err("Unsupported platform".into());
     }
 }
